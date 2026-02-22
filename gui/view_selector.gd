b81ec34537dc583ce356e57f0cd0f9a5ac93edb9
dauntaun <47953199+dauntaun@users.noreply.github.com>
2026-02-22 09:42:42 +0100

    Added grail view selector

--- gui/view_selector.gd
+++ gui/view_selector.gd
@@ -4,36 +4,40 @@ enum Selection {GOBLIN, PD2, GRAIL}
 
 var current_selection: Selection
 
-@onready var goblin_view: Button = %GoblinView
-@onready var pd2_view: Button = %PD2View
-@onready var grail_view: Button = %GrailView
+@onready var goblin_view_button: Button = %GoblinView
+@onready var pd2_view_button: Button = %PD2View
+@onready var grail_view_button: Button = %GrailView
 
-@onready var view_tabs: TabContainer = %ViewSelector
+@onready var view_tab: TabContainer = %ViewSelector
+@onready var main_tab: TabContainer = %MainSelector
+
 @onready var search_bar: SearchBar = %SearchBar
 
-@onready var selection_map: Dictionary[Selection, Button] = {
-	Selection.GOBLIN : goblin_view,
-	Selection.PD2 : pd2_view,
-	Selection.GRAIL : grail_view,
+@onready var selection_map: Dictionary[Selection, Dictionary] = {
+	Selection.GOBLIN: {"button": goblin_view_button, "main_tab": 0, "view_tab": 0},
+	Selection.PD2: {"button": pd2_view_button, "main_tab": 0, "view_tab": 1},
+	Selection.GRAIL: {"button": grail_view_button, "main_tab": 1},
 }
 
 
 func _ready() -> void:
-	selection_map[view_tabs.current_tab].button_pressed = true
+	selection_map[Selection.GOBLIN].button.button_pressed = true
+	_change_view(Selection.GOBLIN)
 	
-	goblin_view.pressed.connect(_change_view.bind(Selection.GOBLIN))
-	pd2_view.pressed.connect(_change_view.bind(Selection.PD2))
-	grail_view.pressed.connect(_change_view.bind(Selection.GRAIL))
-	view_tabs.tab_changed.connect(_reset_selection)
+	goblin_view_button.pressed.connect(_change_view.bind(Selection.GOBLIN))
+	pd2_view_button.pressed.connect(_change_view.bind(Selection.PD2))
+	grail_view_button.pressed.connect(_change_view.bind(Selection.GRAIL))
+	view_tab.tab_changed.connect(_reset_selection)
 
 
 func _change_view(selection: Selection) -> void:
-	view_tabs.current_tab = selection
+	main_tab.current_tab = selection_map[selection].main_tab
+	view_tab.current_tab = selection_map[selection].get("view_tab", 0)
 	search_bar.visible = selection == Selection.GOBLIN
 
 
 func _reset_selection(tab: int) -> void:
 	ItemSelection.clear_selection()
-	var current_view := view_tabs.get_child(tab)
+	var current_view := view_tab.get_child(tab)
 	if current_view.has_method("restore_last_selection"):
 		current_view.restore_last_selection()
