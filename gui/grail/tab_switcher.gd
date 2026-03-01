62d9787e5dc9c3c02921875061f014661bbe7c9f
dauntaun <47953199+dauntaun@users.noreply.github.com>
2026-03-01 13:31:41 +0100

    sync current_tab to buttons

--- gui/grail/tab_switcher.gd
+++ gui/grail/tab_switcher.gd
@@ -1,24 +1,26 @@
+@tool
 class_name TabSwitcher
 extends Control
 
 signal tab_switched(new_tab: int)
 
-enum ButtonMode {MULTIPLE, SINGLE}
-
 @export var switch_buttons: Array[Button]
 @export var tab_container: TabContainer
-@export var button_mode: ButtonMode
 
 
 func _ready() -> void:
 	for i: int in switch_buttons.size():
 		var button := switch_buttons[i]
 		button.toggled.connect(_switch_tab_to_index.bind(i))
+	_sync_tab_to_button(tab_container.current_tab)
+	tab_container.tab_changed.connect(_sync_tab_to_button)
 
 
-func _switch_tab_to_index(toggled: bool, index: int) -> void:
-	if button_mode == ButtonMode.MULTIPLE:
-		tab_container.current_tab = index
-	else:
-		tab_container.current_tab = toggled
+func _switch_tab_to_index(_toggled: bool, index: int) -> void:
+	tab_container.current_tab = index
 	tab_switched.emit(index)
+
+
+func _sync_tab_to_button(tab: int) -> void:
+	for i: int in switch_buttons.size():
+		switch_buttons[i].set_pressed_no_signal(i == tab)
