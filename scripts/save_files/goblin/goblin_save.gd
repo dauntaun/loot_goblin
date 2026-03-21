e7e4e84c44cacffb754cfa039d0f79735e624657
dauntaun <47953199+dauntaun@users.noreply.github.com>
2026-03-21 12:42:49 +0100

    support collections

--- scripts/save_files/goblin/goblin_save.gd
+++ scripts/save_files/goblin/goblin_save.gd
@@ -7,15 +7,9 @@ const FILE_VERSION_BYTE_OFFSET := 4 ## 2 bytes
 const FILE_VERSION_ONE := 1
 const COLLECTIONS_COUNT_BYTE_OFFSET := 48 ## 2 bytes
 const MAIN_COLLECTION_START_BYTE_OFFSET := 50
-
-# Collections header
-const COLLECTION_HEADER := 0x4743 ## GC / 2 bytes
-const COLLECTION_NAME_BYTE_OFFSET := 20 ## Relative to start
-const COLLECTION_NAME_BYTE_LENGTH := 50 ## Null terminates string
 const MAIN_COLLECTION_NAME := "Main"
-const ITEM_LIST_BYTE_OFFSET := 70 ## Relative to start
 
-var item_list: D2ItemList
+var collections: Array[GoblinCollection]
 
 
 func load_file() -> void:
@@ -29,32 +23,29 @@ func load_file() -> void:
 		push_error("Unknown Goblin file version")
 		return
 	var collections_count := data.decode_u16(COLLECTIONS_COUNT_BYTE_OFFSET)
+	if collections_count < 1:
+		push_error("No Goblin collections found in save file")
+		return
 	
 	# Collections parsing
-	var collection_header := data.decode_u16(MAIN_COLLECTION_START_BYTE_OFFSET)
-	if collection_header != COLLECTION_HEADER:
-		push_error("Unknown Goblin collection header")
+	var cursor := BitCursor.new(data, MAIN_COLLECTION_START_BYTE_OFFSET << 3)
+	for i: int in collections_count:
+		var collection := GoblinCollection.from_cursor(cursor)
+		collections.append(collection)
+	
+	if not collections[0].name == MAIN_COLLECTION_NAME:
+		push_error("Unknown main collection")
 		return
-	var collection_name: String = ""
-	for i: int in COLLECTION_NAME_BYTE_LENGTH:
-		var char_code := data.decode_u8(MAIN_COLLECTION_START_BYTE_OFFSET + COLLECTION_NAME_BYTE_OFFSET + i)
-		if char_code == 0:
-			break
-		collection_name += char(char_code)
-	if collection_name != MAIN_COLLECTION_NAME:
-		push_error("Unknown Main collection name")
-		return
-	load_successful = true
-	var cursor := BitCursor.new(data, (MAIN_COLLECTION_START_BYTE_OFFSET + ITEM_LIST_BYTE_OFFSET) << 3)
-	item_list = D2ItemList.new(cursor)
+	
+	load_successful= true
 
 
 func save_file(path: String) -> void:
 	var file := FileAccess.open(path, FileAccess.WRITE)
 	if data.is_empty():
 		write_header()
-	if item_list:
-		item_list.write_item_count()
+	for collection: GoblinCollection in collections:
+		collection.item_list.write_item_count()
 	file.store_buffer(data)
 	file.close()
 	save_timestamp = FileAccess.get_modified_time(path)
@@ -68,12 +59,71 @@ func write_header() -> void:
 	data.encode_u16(FILE_VERSION_BYTE_OFFSET, FILE_VERSION_ONE)
 	data.encode_u16(COLLECTIONS_COUNT_BYTE_OFFSET, 1)
 	# Main collection header
-	data.encode_u16(MAIN_COLLECTION_START_BYTE_OFFSET, COLLECTION_HEADER)
-	# Encode name
-	var name_bytes := MAIN_COLLECTION_NAME.to_ascii_buffer()
-	if name_bytes.size() != COLLECTION_NAME_BYTE_LENGTH:
-		name_bytes.append(0)
-	for i: int in name_bytes.size():
-		data.encode_u8(MAIN_COLLECTION_START_BYTE_OFFSET + COLLECTION_NAME_BYTE_OFFSET + i, name_bytes[i])
+	var main := GoblinCollection.new(MAIN_COLLECTION_START_BYTE_OFFSET, data)
+	main.write_header()
+	main.name = MAIN_COLLECTION_NAME
+	main.write_name()
+	main.write_itemlist_header()
+
+
+class GoblinCollection:
+	const COLLECTION_HEADER := 0x4743 ## GC / 2 bytes
+	const COLLECTION_NAME_BYTE_OFFSET := 20 ## Relative to start
+	const COLLECTION_NAME_BYTE_LENGTH := 50 ## Null terminates string
+	const ITEM_LIST_BYTE_OFFSET := 70 ## Relative to start
 	
-	data.encode_u16(MAIN_COLLECTION_START_BYTE_OFFSET + ITEM_LIST_BYTE_OFFSET, ItemParser.ITEM_SIGNATURE)
+	static var collection_id_counter := 0
+	
+	var collection_id: int
+	var name: String
+	var item_list: D2ItemList
+	
+	var _start_byte: int
+	var _data: PackedByteArray
+	
+	
+	func _init(start_byte: int, data: PackedByteArray) -> void:
+		_start_byte = start_byte
+		_data = data
+		collection_id = collection_id_counter
+		collection_id_counter += 1
+	
+	
+	static func from_cursor(cursor: BitCursor) -> GoblinCollection:
+		var start_bit := cursor._bit_pos
+		var collection := GoblinCollection.new(start_bit << 3, cursor._data)
+		var collection_header := cursor.read_bits(2 << 3)
+		if collection_header != COLLECTION_HEADER:
+			push_error("Unknown Goblin collection header")
+			return
+		cursor.set_at(start_bit + (COLLECTION_NAME_BYTE_OFFSET << 3))
+		var collection_name: String = ""
+		for i: int in COLLECTION_NAME_BYTE_LENGTH:
+			var char_code := cursor.read_bits(8)
+			if char_code == 0:
+				break
+			collection_name += char(char_code)
+		collection.name = collection_name
+		cursor.set_at(start_bit + (ITEM_LIST_BYTE_OFFSET << 3))
+		collection.item_list = D2ItemList.new(cursor)
+		return collection
+	
+	
+	func write_header() -> void:
+		_data.encode_u16(_start_byte, COLLECTION_HEADER)
+	
+	
+	func write_name() -> void:
+		var name_bytes := name.to_ascii_buffer()
+		if name_bytes.size() != COLLECTION_NAME_BYTE_LENGTH:
+			name_bytes.append(0)
+		for i: int in name_bytes.size():
+			_data.encode_u8(_start_byte + COLLECTION_NAME_BYTE_OFFSET + i, name_bytes[i])
+	
+	
+	func write_itemlist_header() -> void:
+		_data.encode_u16(_start_byte + ITEM_LIST_BYTE_OFFSET, ItemParser.ITEM_SIGNATURE)
+	
+	
+	
+	
