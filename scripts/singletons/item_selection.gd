a7b7f12ec55ed8493f7489697b497e51c8184cdd
dauntaun <47953199+dauntaun@users.noreply.github.com>
2026-03-21 10:43:55 +0100

    Use stash IDs and add destination signals

--- scripts/singletons/item_selection.gd
+++ scripts/singletons/item_selection.gd
@@ -5,42 +5,56 @@ enum TransferMode {SINGLE, BULK}
 # Source changed
 signal selection_changed
 # Destination changed
-signal destination_changed
+signal destination_changed(new_stash_type: StashRegistry.StashType)
+signal active_page_changed(new_page: int)
+signal active_collection_changed(new_collection_id: int)
+signal active_character_changed(new_character_id: int)
 signal transfer_mode_changed
-signal active_page_changed
 # Transfer occurred
-signal items_transferred(from: StashRegistry.StashType, to: StashRegistry.StashType)
+signal items_transferred(from_stash_id: int, to_stash_id: int)
 signal stash_cleared
 signal plugy_imported
 
 # Source state
 var _selected_item: D2Item
 var _bulk_selection: Array[D2Item]
-var _source_stash: StashRegistry.StashType
+var _source_stash_id := -1
 # Destination state
-var _destination_stash: StashRegistry.StashType
-var _destination_page_index: int
+var _destination_stash_type: StashRegistry.StashType
+var _destination_page_index := 8
+var _destination_collection_id := -1
+var _destination_character_id := -1
 var _transfer_mode: TransferMode
 
 
-func set_selection(item: D2Item, stash: StashRegistry.StashType, bulk_items: Array[D2Item] = []) -> void:
-	if _selected_item != item or _source_stash != stash or _bulk_selection != bulk_items:
+func set_selection(item: D2Item, stash_id: int, bulk_items: Array[D2Item] = []) -> void:
+	if _selected_item != item or _source_stash_id != stash_id or _bulk_selection != bulk_items:
 		_selected_item = item
 		_bulk_selection = bulk_items
-		if stash == StashRegistry.StashType.GOBLIN:
-			set_destination_stash(StashRegistry.StashType.PD2_SHARED)
-		elif stash == StashRegistry.StashType.PD2_SHARED and \
-			_source_stash in [StashRegistry.StashType.GOBLIN, StashRegistry.StashType.UNKNOWN]:
-			set_destination_stash(StashRegistry.StashType.GOBLIN)
-		_source_stash = stash
+		var source_stash_type := StashRegistry.get_stash_type(_source_stash_id)
+		var new_stash_type := StashRegistry.get_stash_type(stash_id)
+		if new_stash_type == StashRegistry.StashType.GOBLIN and \
+			_destination_stash_type in [StashRegistry.StashType.GOBLIN, StashRegistry.StashType.UNKNOWN]:
+			set_destination(StashRegistry.StashType.PD2_SHARED)
+		elif new_stash_type in [StashRegistry.StashType.PD2_SHARED, StashRegistry.StashType.PD2_PERSONAL] and \
+			source_stash_type in [StashRegistry.StashType.GOBLIN, StashRegistry.StashType.UNKNOWN]:
+			set_destination(StashRegistry.StashType.GOBLIN)
+		_source_stash_id = stash_id
 		selection_changed.emit()
 
 
 func clear_selection() -> void:
 	if _selected_item:
+		_source_stash_id = -1
 		_selected_item = null
 		_bulk_selection = []
 		selection_changed.emit()
+
+
+func clear_destination() -> void:
+	_destination_collection_id = -1
+	_destination_character_id = -1
+	_destination_stash_type = StashRegistry.StashType.UNKNOWN
 
 
 func get_last_selected_item() -> D2Item:
@@ -61,49 +75,69 @@ func get_selected_items() -> Array[D2Item]:
 		return _bulk_selection
 
 
-func get_source_stash_type() -> StashRegistry.StashType:
-	return _source_stash
-
-
 func is_goblin_selected() -> bool:
-	return _source_stash == StashRegistry.StashType.GOBLIN
+	return StashRegistry.get_stash_type(_source_stash_id) == StashRegistry.StashType.GOBLIN
 
 
 func is_pd2_shared_selected() -> bool:
-	return _source_stash == StashRegistry.StashType.PD2_SHARED
+	return StashRegistry.get_stash_type(_source_stash_id) == StashRegistry.StashType.PD2_SHARED
 
 
 func is_pd2_personal_selected() -> bool:
-	return _source_stash == StashRegistry.StashType.PD2_PERSONAL
+	return StashRegistry.get_stash_type(_source_stash_id) == StashRegistry.StashType.PD2_PERSONAL
 
 
 func is_pd2_materials_selected() -> bool:
-	return _source_stash == StashRegistry.StashType.PD2_MATERIALS
+	return StashRegistry.get_stash_type(_source_stash_id) == StashRegistry.StashType.PD2_MATERIALS
 
 
 func set_destination_page_index(page_index: int) -> void:
 	page_index = clamp(page_index, 0, 8)
 	if _destination_page_index != page_index:
 		_destination_page_index = page_index
-		active_page_changed.emit()
+		active_page_changed.emit(page_index)
+
+
+func set_destination_character(character_id: int) -> void:
+	if _destination_character_id != character_id:
+		_destination_character_id = character_id
+		active_character_changed.emit(character_id)
+
+
+func set_destination_collection(collection_id: int) -> void:
+	if _destination_collection_id != collection_id:
+		_destination_collection_id = collection_id
+		active_collection_changed.emit(collection_id)
+
+
+func get_destination_stash_id() -> int:
+	match _destination_stash_type:
+		StashRegistry.StashType.GOBLIN:
+			return StashRegistry.get_goblin_stash_id(_destination_collection_id)
+		StashRegistry.StashType.PD2_SHARED:
+			return StashRegistry.get_pd2_shared_stash_id()
+		StashRegistry.StashType.PD2_PERSONAL:
+			return StashRegistry.get_character_stash_id(_destination_character_id)
+		_:
+			return -1
 
 
 func get_destination_page_index() -> int:
 	return _destination_page_index
 
 
-func set_destination_stash(new_stash: StashRegistry.StashType) -> void:
-	if _destination_stash != new_stash:
-		_destination_stash = new_stash
-		destination_changed.emit()
+func get_destination_character_id() -> int:
+	return _destination_character_id
 
 
-func is_destination_goblin() -> bool:
-	return _destination_stash == StashRegistry.StashType.GOBLIN
+func get_destination_collection_id() -> int:
+	return _destination_collection_id
 
 
-func is_destination_pd2_shared() -> bool:
-	return _destination_stash == StashRegistry.StashType.PD2_SHARED
+func set_destination(stash_type: StashRegistry.StashType) -> void:
+	if _destination_stash_type != stash_type:
+		_destination_stash_type = stash_type
+		destination_changed.emit(stash_type)
 
 
 func set_transfer_mode(new_mode: TransferMode) -> void:
@@ -114,13 +148,16 @@ func set_transfer_mode(new_mode: TransferMode) -> void:
 # ===== ItemTransfer =====
 
 func store_active_page() -> void:
-	var pd2_view := StashRegistry.get_stash_view(StashRegistry.StashType.PD2_SHARED) as PagedStashView
-	if pd2_view:
-		var items := pd2_view.get_items_in_page(_destination_page_index).duplicate()
-		for item: D2Item in items:
-			var transfer_record := CommandQueue.ItemTransferCommand.new(item, StashRegistry.StashType.PD2_SHARED, StashRegistry.StashType.GOBLIN)
-			CommandQueue.record_command(transfer_record)
-			items_transferred.emit(StashRegistry.StashType.PD2_SHARED, StashRegistry.StashType.GOBLIN)
+	var pd2_stash_id := StashRegistry.get_pd2_shared_stash_id()
+	var goblin_stash_id := StashRegistry.get_goblin_main_stash_id()
+	if pd2_stash_id == -1 or goblin_stash_id == -1:
+		return
+	var pd2_view := StashRegistry.get_stash_view(pd2_stash_id) as PagedStashView
+	var items := pd2_view.get_items_in_page(_destination_page_index).duplicate()
+	for item: D2Item in items:
+		var transfer_record := CommandQueue.ItemTransferCommand.new(item, pd2_stash_id, goblin_stash_id)
+		CommandQueue.record_command(transfer_record)
+		items_transferred.emit(pd2_stash_id, goblin_stash_id)
 
 
 func transfer_selection() -> void:
@@ -131,40 +168,51 @@ func transfer_selection() -> void:
 
 
 func transfer_items() -> void:
-	var stash_view := StashRegistry.get_stash_view(_destination_stash)
+	var destination_stash_id := get_destination_stash_id()
+	var stash_view := StashRegistry.get_stash_view(destination_stash_id)
 	
 	for item: D2Item in get_selected_items().duplicate():
 		var placement := stash_view.get_placement(item, _destination_page_index)
-		var transfer_record := CommandQueue.ItemTransferCommand.new(item, _source_stash, _destination_stash, placement)
+		var transfer_record := CommandQueue.ItemTransferCommand.new(item, _source_stash_id, destination_stash_id, placement)
 		CommandQueue.record_command(transfer_record)
 	
+	var source_id := _source_stash_id
 	clear_selection()
-	items_transferred.emit(_source_stash, _destination_stash)
+	items_transferred.emit(source_id, destination_stash_id)
 
 
 func can_transfer_selection() -> bool:
-	var from_stash := StashRegistry.get_stash_entry(_source_stash)
-	var to_stash := StashRegistry.get_stash_entry(_destination_stash)
-	
-	if not from_stash or not to_stash or get_selected_items().is_empty():
-		return false
+	var destination_stash_id := get_destination_stash_id()
+	var selected_items := get_selected_items()
+	if not StashRegistry.has_stash(_source_stash_id) \
+		or not StashRegistry.has_stash(destination_stash_id) \
+		or selected_items.is_empty():
+			return false
 	if CommandQueue.is_command_queue_blocked():
 		return false
-	if not to_stash.view.can_add_items(get_selected_items(), _destination_page_index):
+	var source_stash_type := StashRegistry.get_stash_type(_source_stash_id)
+	if source_stash_type == StashRegistry.StashType.PD2_SHARED and _destination_stash_type == source_stash_type:
+		if selected_items[0].equipped_id == _destination_page_index + 1:
+			return false
+	elif source_stash_type in [StashRegistry.StashType.GOBLIN, StashRegistry.StashType.PD2_PERSONAL] and _destination_stash_type == source_stash_type:
+		if _source_stash_id == destination_stash_id:
+			return false
+	var destination_view := StashRegistry.get_stash_view(destination_stash_id)
+	if not destination_view.can_add_items(selected_items, _destination_page_index, D2Item.ItemLocation.STORED, D2Item.StoreLocation.STASH):
 		return false
 	else:
 		return true
 
 
 func clear_goblin_stash() -> void:
-	var clear_command := CommandQueue.StashClearCommand.new(StashRegistry.StashType.GOBLIN)
+	var clear_command := CommandQueue.StashClearCommand.new(StashRegistry.get_goblin_main_stash_id())
 	CommandQueue.record_command(clear_command)
 	clear_selection()
 	stash_cleared.emit()
 
 
 func import_plugy_items(item_lists: Array[D2ItemList]) -> void:
-	var import_command := CommandQueue.ImportPlugyCommand.new(item_lists, StashRegistry.StashType.GOBLIN)
+	var import_command := CommandQueue.ImportPlugyCommand.new(item_lists, StashRegistry.get_goblin_main_stash_id())
 	CommandQueue.record_command(import_command)
 	clear_selection()
 	plugy_imported.emit()
