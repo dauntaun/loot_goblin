4f8146e0f2d35331faf7906556a3822d1b24dc27
dauntaun <47953199+dauntaun@users.noreply.github.com>
2026-03-21 10:42:47 +0100

    Refactor item registry mappings and add unregisters

--- scripts/singletons/item_registry.gd
+++ scripts/singletons/item_registry.gd
@@ -1,19 +1,38 @@
 # ItemRegistry
 extends Node
 
-var item_data_register: Dictionary[int, D2ItemList]
-var item_view_register: Dictionary[int, BasicStashView]
-var item_register: Dictionary[int, D2Item]
+var item_register: Dictionary[int, D2Item] ## item_id -> D2Item
+var item_data_register: Dictionary[int, int] ## item_id -> stash_id (d2itemlist)
+var item_view_register: Dictionary[int, int] ## item_id -> stash_id (view)
 
 
-func get_item_data(item_id: int) -> D2ItemList:
+func get_item_data_stash_id(item_id: int) -> int:
 	return item_data_register[item_id]
 
 
-func get_item_view(item_id: int) -> BasicStashView:
+func get_item_view_stash_id(item_id: int) -> int:
 	return item_view_register[item_id]
 
 
-func get_item_stash(item_id: int) -> StashRegistry.StashType:
-	var source := get_item_data(item_id)
-	return StashRegistry.get_stash_type_from_data(source)
+func unregister_all_items() -> void:
+	item_register.clear()
+	item_data_register.clear()
+	item_view_register.clear()
+
+
+func unregister_items_from_stash(stash_id: int) -> void:
+	var to_remove: Array[int]
+
+	for item_id: int in item_data_register:
+		if item_data_register[item_id] == stash_id:
+			to_remove.append(item_id)
+
+	for item_id: int in item_view_register:
+		if item_view_register[item_id] == stash_id:
+			if not to_remove.has(item_id):
+				to_remove.append(item_id)
+
+	for item_id: int in to_remove:
+		item_register.erase(item_id)
+		item_data_register.erase(item_id)
+		item_view_register.erase(item_id)
